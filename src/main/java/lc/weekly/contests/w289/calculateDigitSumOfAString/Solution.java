a389828b9900439e063336ef58bddf920d419b8c
sherlockyb <yb_cswhu@aliyun.com>
2022-04-25 23:02:06 +0800

    ADD: w289

---
+++ src/main/java/lc/weekly/contests/w289/calculateDigitSumOfAString/Solution.java
@@ -0,0 +1,11 @@
+package lc.weekly.contests.w289.calculateDigitSumOfAString;
+
+/**
+ * @author sherlockyb
+ */
+public class Solution {
+    public String digitSum(String s, int k) {
+        // TODO
+        return null;
+    }
+}
