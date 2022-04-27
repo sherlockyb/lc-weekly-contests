fcdff193cfee0880e39e0297e83003b6f6d1a85f
sherlockyb <yb_cswhu@aliyun.com>
2022-04-27 19:35:45 +0800

    ADD: solution for calculateDigitSumOfAString

--- src/main/java/lc/weekly/contests/w289/calculateDigitSumOfAString/Solution.java
+++ src/main/java/lc/weekly/contests/w289/calculateDigitSumOfAString/Solution.java
@@ -5,7 +5,30 @@ package lc.weekly.contests.w289.calculateDigitSumOfAString;
  */
 public class Solution {
     public String digitSum(String s, int k) {
-        // TODO
-        return null;
+        if (s == null) {
+            return null;
+        }
+
+        final StringBuilder sb = new StringBuilder(s.length());
+        while (s.length() > k) {
+            int sum = 0;
+            for (int i = 0; i < s.length(); i++) {
+                sum += s.charAt(i) - '0';
+                if ((i + 1) % k == 0) {
+                    sb.append(sum);
+                    sum = 0;
+                }
+            }
+
+            // last items less then k
+            if (s.length() % k != 0) {
+                sb.append(sum);
+            }
+
+            s = sb.toString();
+            sb.delete(0, sb.length());
+        }
+
+        return s;
     }
 }
