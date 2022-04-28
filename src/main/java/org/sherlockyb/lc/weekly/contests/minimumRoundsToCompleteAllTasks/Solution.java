89e5616fe0f2626e52696c79371ccbfe1744b4ae
sherlockyb <yb_cswhu@aliyun.com>
2022-04-28 21:58:21 +0800

    ADD: README and ideal for minimumRoundsToCompleteAllTasks

---
+++ src/main/java/org/sherlockyb/lc/weekly/contests/minimumRoundsToCompleteAllTasks/Solution.java
@@ -0,0 +1,11 @@
+package org.sherlockyb.lc.weekly.contests.minimumRoundsToCompleteAllTasks;
+
+/**
+ * @author sherlockyb
+ */
+public class Solution {
+    public int minimumRounds(int[] tasks) {
+        // TODO
+        return 0;
+    }
+}
